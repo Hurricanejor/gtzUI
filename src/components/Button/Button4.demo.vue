ce3f4620a2215bb35457a4e8bb7b451411723c42
hurricanejor <m18837123805@163.com>
2020-10-17 20:49:01 +0800

    Button组件实现代码查看

---
+++ src/components/Button/Button4.demo.vue
@@ -0,0 +1,26 @@
+<demo>
+    示例4
+</demo>
+<template>
+    <Button disabled>禁用按钮</Button>
+    <Button theme="link" disabled>禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Button from '../../lib/Button.vue';
+export default ({
+    components: {
+        Button
+    },
+    setup(props) {
+        const onClick = () => {
+            console.log(`HI`);
+        }
+        return {
+            onClick
+        }
+    }
+})
+</script>
