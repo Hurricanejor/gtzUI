07332ee7df14971e6ae64d81b5eb440950e877bc
hurricanejor <m18837123805@163.com>
2020-10-20 19:02:48 +0800

    代码高亮显示bug修改

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,17 +1,18 @@
 <template>
-  <article class="markdown-body">
-  <h2>Button按钮</h2>
-   
-  <p>基础组件，触发业务逻辑时使用。</p>
-  <h2>代码示例</h2>
-  <Demo :componentName="Button1Demo"></Demo>
-  <Demo :componentName="Button2Demo"></Demo>
-  <Demo :componentName="Button3Demo"></Demo>
-  <Demo :componentName="Button4Demo"></Demo>
-  <Demo :componentName="Button5Demo"></Demo>
-  </article>
+  <div>
+    <h2>Button按钮</h2>
+    
+    <p>基础组件，触发业务逻辑时使用。</p>
+    <h2>代码示例</h2>
+    <Demo :componentName="Button1Demo"></Demo>
+    <Demo :componentName="Button2Demo"></Demo>
+    <Demo :componentName="Button3Demo"></Demo>
+    <Demo :componentName="Button4Demo"></Demo>
+    <Demo :componentName="Button5Demo"></Demo>
+  </div>
 </template>
 <script lang="ts">
+import {ref} from 'vue'
 import Demo from "./Demo.vue";
 import Button1Demo from "./Button/Button1.demo.vue";
 import Button2Demo from "./Button/Button2.demo.vue";
--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -54,6 +54,8 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
+  border-radius: 5px;
+  box-shadow: 0px 5px 20px #dcdcdc;
   >h2 {
     font-size: 20px;
     padding: 8px 16px;
